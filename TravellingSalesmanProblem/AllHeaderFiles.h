d18c2008a39679ae32a600fd990f3f861d82af75
Dvdhyh <dvdhyh1@gmail.com>
2016-05-27 17:29:24 +1000

    A

---
+++ TravellingSalesmanProblem/AllHeaderFiles.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef ALLHEADERFILES_H
+#define ALLHEADERFILES_H
+
+#include <windows.h>
+
+
+#include <vector>
+#include <map>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <random>
+
+#include <objidl.h>  //Color
+#include <gdiplus.h>
+#include <stdio.h>
+
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+
+#include "C:\Users\DavidHuynh\Documents\Visual Studio 2015\MyHeaders\GUI.h"
+
+#include "TSPGeneticAlgorithm.h"
+#include "PaintScreen.h"
+
+#endif // !ALLHEADERFILES
